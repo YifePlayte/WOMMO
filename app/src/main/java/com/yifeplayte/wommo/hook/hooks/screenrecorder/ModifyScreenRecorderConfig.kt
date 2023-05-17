8ad1f04bc68f9b0433faf6452502ff099b084a0a
YifePlayte <yife-playte@foxmail.com>
2023-05-17 15:21:54 +0800

    Update ModifyScreenRecorderConfig.kt

--- app/src/main/java/com/yifeplayte/wommo/hook/hooks/screenrecorder/ModifyScreenRecorderConfig.kt
+++ app/src/main/java/com/yifeplayte/wommo/hook/hooks/screenrecorder/ModifyScreenRecorderConfig.kt
@@ -12,11 +12,8 @@ object ModifyScreenRecorderConfig : BaseHook() {
         loadDexKit()
         dexKitBridge.findMethodUsingString {
             usingString = "Error when set frame value, maxValue = "
-        }.map {
-            it.getMethodInstance(EzXHelper.safeClassLoader)
-        }.firstOrNull {
-            it.parameterCount == 2 && it.parameterTypes[0] == Int::class.java && it.parameterTypes[1] == Int::class.java
-        }?.createHook {
+            methodParamTypes = arrayOf("I", "I")
+        }.firstOrNull()?.getMethodInstance(EzXHelper.safeClassLoader)?.createHook {
             before { param ->
                 param.args[0] = 3600
                 param.args[1] = 1
@@ -24,12 +21,11 @@ object ModifyScreenRecorderConfig : BaseHook() {
                     field.also {
                         it.isAccessible = true
                     }.let { fieldAccessible ->
-                        fieldAccessible.isFinal &&
-                                fieldAccessible.get(null).let {
-                                    kotlin.runCatching {
-                                        (it as IntArray).contentEquals(intArrayOf(15, 24, 30, 48, 60, 90))
-                                    }.getOrDefault(false)
-                                }
+                        fieldAccessible.isFinal && fieldAccessible.get(null).let {
+                            kotlin.runCatching {
+                                (it as IntArray).contentEquals(intArrayOf(15, 24, 30, 48, 60, 90))
+                            }.getOrDefault(false)
+                        }
                     }
                 }?.set(null, intArrayOf(15, 24, 30, 48, 60, 90, 120, 144))
             }
@@ -48,12 +44,11 @@ object ModifyScreenRecorderConfig : BaseHook() {
                     field.also {
                         it.isAccessible = true
                     }.let { fieldAccessible ->
-                        fieldAccessible.isFinal &&
-                                fieldAccessible.get(null).let {
-                                    kotlin.runCatching {
-                                        (it as IntArray).contentEquals(intArrayOf(200, 100, 50, 32, 24, 16, 8, 6, 4, 1))
-                                    }.getOrDefault(false)
-                                }
+                        fieldAccessible.isFinal && fieldAccessible.get(null).let {
+                            kotlin.runCatching {
+                                (it as IntArray).contentEquals(intArrayOf(200, 100, 50, 32, 24, 16, 8, 6, 4, 1))
+                            }.getOrDefault(false)
+                        }
                     }
                 }?.set(null, intArrayOf(3600, 2400, 1200, 800, 400, 200, 100, 50, 32, 24, 16, 8, 6, 4, 1))
             }
