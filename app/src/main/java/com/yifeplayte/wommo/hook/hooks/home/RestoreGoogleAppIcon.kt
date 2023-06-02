fd137057d1db792bd44893490fcb5b8245806929
YifePlayte <yife-playte@foxmail.com>
2023-06-02 15:48:30 +0800

    Fix restore Google app icon

--- app/src/main/java/com/yifeplayte/wommo/hook/hooks/home/RestoreGoogleAppIcon.kt
+++ app/src/main/java/com/yifeplayte/wommo/hook/hooks/home/RestoreGoogleAppIcon.kt
@@ -3,20 +3,15 @@ package com.yifeplayte.wommo.hook.hooks.home
 import android.content.ComponentName
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHooks
-import com.github.kyuubiran.ezxhelper.ObjectHelper.Companion.objectHelper
+import com.github.kyuubiran.ezxhelper.ObjectUtils.getObjectOrNullAs
 import com.yifeplayte.wommo.hook.hooks.BaseHook
 
 object RestoreGoogleAppIcon : BaseHook() {
     override fun init() {
-        loadClass("com.miui.home.launcher.AppFilter").constructors.createHooks {
+        loadClass("com.miui.home.launcher.AppFilter").declaredConstructors.createHooks {
             after { param ->
-                param.thisObject.objectHelper {
-                    getObjectOrNullAs<HashSet<ComponentName>>("mSkippedItems")?.removeIf {
-                        it.packageName in listOf(
-                            "com.google.android.googlequicksearchbox",
-                            "com.google.android.gms"
-                        )
-                    }
+                getObjectOrNullAs<HashSet<ComponentName>>(param.thisObject, "mSkippedItems")!!.removeIf {
+                    it.packageName in setOf("com.google.android.googlequicksearchbox", "com.google.android.gms")
                 }
             }
         }
