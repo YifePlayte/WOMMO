a612f9731e02a15e37b14ec04a10b2b7d9cb03e1
YifePlayte <yife-playte@foxmail.com>
2023-06-28 22:41:34 +0800

    Fix ForceDarkModeForAllApps for some miui and google apps

--- app/src/main/java/com/yifeplayte/wommo/hook/hooks/singlepackage/android/ForceDarkModeForAllApps.kt
+++ app/src/main/java/com/yifeplayte/wommo/hook/hooks/singlepackage/android/ForceDarkModeForAllApps.kt
@@ -1,8 +1,10 @@
 package com.yifeplayte.wommo.hook.hooks.singlepackage.android
 
+import android.content.pm.ApplicationInfo
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
 import com.github.kyuubiran.ezxhelper.ClassUtils.setStaticObject
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHooks
+import com.github.kyuubiran.ezxhelper.ObjectUtils.invokeMethodBestMatch
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import com.yifeplayte.wommo.hook.hooks.BaseHook
 import com.yifeplayte.wommo.utils.Build.IS_INTERNATIONAL_BUILD
@@ -12,14 +14,21 @@ object ForceDarkModeForAllApps : BaseHook() {
     override val key = "force_dark_mode_for_all_apps"
     override fun hook() {
         if (IS_INTERNATIONAL_BUILD) return
-        loadClass("com.android.server.ForceDarkAppListManager").methodFinder().filterByName("getDarkModeAppList")
-            .toList().createHooks {
-                before {
-                    setStaticObject(loadClass("miui.os.Build"), "IS_INTERNATIONAL_BUILD", true)
-                }
-                after {
-                    setStaticObject(loadClass("miui.os.Build"), "IS_INTERNATIONAL_BUILD", IS_INTERNATIONAL_BUILD)
-                }
+        val clazzForceDarkAppListManager = loadClass("com.android.server.ForceDarkAppListManager")
+        clazzForceDarkAppListManager.methodFinder().filterByName("getDarkModeAppList").toList().createHooks {
+            before {
+                setStaticObject(loadClass("miui.os.Build"), "IS_INTERNATIONAL_BUILD", true)
             }
+            after {
+                setStaticObject(loadClass("miui.os.Build"), "IS_INTERNATIONAL_BUILD", IS_INTERNATIONAL_BUILD)
+            }
+        }
+        clazzForceDarkAppListManager.methodFinder().filterByName("shouldShowInSettings").toList().createHooks {
+            before { param ->
+                val info = param.args[0] as ApplicationInfo?
+                param.result =
+                    !(info == null || (invokeMethodBestMatch(info, "isSystemApp") as Boolean) || info.uid < 10000)
+            }
+        }
     }
 }
