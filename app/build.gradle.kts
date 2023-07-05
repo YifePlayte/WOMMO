f3050b03d55529bcaf0168f31c00dc849db5719d
YifePlayte <yife-playte@foxmail.com>
2023-07-05 14:28:42 +0800

    Optimize code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
 }
 
 dependencies {
-    implementation(project(":blockmiui"))
+    compileOnly("de.robv.android.xposed:api:82")
     implementation("com.github.kyuubiran:EzXHelper:2.0.6")
     implementation("org.luckypray:DexKit:1.1.8")
-    compileOnly("de.robv.android.xposed:api:82")
+    implementation(project(":blockmiui"))
 }
