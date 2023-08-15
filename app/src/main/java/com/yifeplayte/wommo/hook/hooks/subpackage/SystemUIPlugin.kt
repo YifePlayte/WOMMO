502eaaa14568c8c0502e8e5d2054f31c4d722cb7
YifePlayte <yife-playte@foxmail.com>
2023-08-15 19:45:59 +0800

    Using unhook to optimize performance

--- app/src/main/java/com/yifeplayte/wommo/hook/hooks/subpackage/SystemUIPlugin.kt
+++ app/src/main/java/com/yifeplayte/wommo/hook/hooks/subpackage/SystemUIPlugin.kt
@@ -6,6 +6,7 @@ import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import com.yifeplayte.wommo.hook.hooks.BaseSubPackage
 import com.yifeplayte.wommo.hook.hooks.subpackage.systemuiplugin.RestoreNearbyTile
+import de.robv.android.xposed.XC_MethodHook.Unhook
 
 object SystemUIPlugin : BaseSubPackage() {
     override val packageName = "com.android.systemui"
@@ -13,15 +14,16 @@ object SystemUIPlugin : BaseSubPackage() {
     override val hooks = setOf(
         RestoreNearbyTile
     )
-
+    var hook: Unhook? = null
     override fun initClassLoader() {
-        loadClass("com.android.systemui.shared.plugins.PluginInstance\$Factory").methodFinder()
+        hook = loadClass("com.android.systemui.shared.plugins.PluginInstance\$Factory").methodFinder()
             .filterByName("getClassLoader")
             .filterByAssignableParamTypes(ApplicationInfo::class.java, ClassLoader::class.java)
             .first().createHook {
                 after { param ->
                     if ((param.args[0] as ApplicationInfo).packageName != subPackageName) return@after
                     safeSubClassLoader = param.result as? ClassLoader ?: return@after
+                    hook?.unhook()
                 }
             }
     }
