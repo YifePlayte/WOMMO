fac42f7da633e2169e15b45402f0d0336255306d
YifePlayte <yife-playte@foxmail.com>
2023-08-17 01:21:17 +0800

    Fix RemoveXlDownload

--- app/src/main/java/com/yifeplayte/wommo/hook/hooks/singlepackage/downloadprovider/RemoveXlDownload.kt
+++ app/src/main/java/com/yifeplayte/wommo/hook/hooks/singlepackage/downloadprovider/RemoveXlDownload.kt
@@ -1,22 +1,24 @@
 package com.yifeplayte.wommo.hook.hooks.singlepackage.downloadprovider
 
-import android.os.Environment
-import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
+import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
+import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHooks
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import com.yifeplayte.wommo.hook.hooks.BaseHook
-import java.io.File
-import java.io.FileNotFoundException
 
 object RemoveXlDownload : BaseHook() {
     override val key = "remove_xl_download"
     override fun hook() {
-        val targetPath = File(Environment.getExternalStorageDirectory(), ".xlDownload").absoluteFile
-        File::class.java.methodFinder().filterByName("mkdirs").first().createHook {
-            before {
-                if ((it.thisObject as File).absoluteFile.equals(targetPath)) {
-                    it.throwable = FileNotFoundException("blocked")
-                }
-            }
+        val clazzXLConfig = loadClass("com.android.providers.downloads.config.XLConfig")
+        clazzXLConfig.methodFinder().filter { name in setOf("setDebug", "setSoDebug") }.toList().createHooks {
+            returnConstant(null)
         }
+        // val targetPath = File(Environment.getExternalStorageDirectory(), ".xlDownload").absoluteFile
+        // File::class.java.methodFinder().filterByName("mkdirs").first().createHook {
+        //     before {
+        //         if ((it.thisObject as File).absoluteFile.equals(targetPath)) {
+        //             it.throwable = FileNotFoundException("blocked")
+        //         }
+        //     }
+        // }
     }
 }
