1da90f5c314644d315355a1c43799dc081156173
YifePlayte <62252171+YifePlayte@users.noreply.github.com>
2023-12-09 23:38:18 +0800

    Optimize BaseUniversalHook

--- app/src/main/java/com/yifeplayte/wommo/hook/hooks/BaseUniversalHook.kt
+++ app/src/main/java/com/yifeplayte/wommo/hook/hooks/BaseUniversalHook.kt
@@ -1,5 +1,6 @@
 package com.yifeplayte.wommo.hook.hooks
 
+import com.github.kyuubiran.ezxhelper.EzXHelper.hostPackageName
 import com.github.kyuubiran.ezxhelper.Log
 import com.github.kyuubiran.ezxhelper.LogExtensions.logexIfThrow
 import com.yifeplayte.wommo.hook.utils.XSharedPreferences.getBoolean
@@ -8,8 +9,14 @@ abstract class BaseUniversalHook {
     abstract val key: String
     abstract fun hook()
     open val isEnabled get() = getBoolean(key, false)
+    open val isPartialUniversalHook: Boolean = false
+    open val scope: Set<String>? = null
     fun init() {
         if (isEnabled) runCatching {
+            if (isPartialUniversalHook) {
+                if (scope == null) return
+                if (hostPackageName !in scope!!) return
+            }
             hook()
             Log.ix("Inited universal hook: ${this.javaClass.simpleName}")
         }.logexIfThrow("Failed init universal hook: ${this.javaClass.simpleName}")
