ed5c0935629b7ff8c07c29f47ff00f8ea79486eb
YifePlayte <62252171+YifePlayte@users.noreply.github.com>
2024-01-28 02:02:35 +0800

    Fix ShowNotificationHistoryAndLogEntry for HyperOS

--- app/src/main/java/com/yifeplayte/wommo/hook/hooks/singlepackage/settings/ShowNotificationHistoryAndLogEntry.kt
+++ app/src/main/java/com/yifeplayte/wommo/hook/hooks/singlepackage/settings/ShowNotificationHistoryAndLogEntry.kt
@@ -7,7 +7,9 @@ import android.content.res.Resources
 import android.util.AttributeSet
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
 import com.github.kyuubiran.ezxhelper.EzXHelper.hostPackageName
+import com.github.kyuubiran.ezxhelper.HookFactory
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
+import com.github.kyuubiran.ezxhelper.ObjectHelper.Companion.objectHelper
 import com.github.kyuubiran.ezxhelper.ObjectUtils.invokeMethodBestMatch
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import com.yifeplayte.wommo.hook.hooks.BaseHook
@@ -15,55 +17,69 @@ import com.yifeplayte.wommo.hook.hooks.BaseHook
 object ShowNotificationHistoryAndLogEntry : BaseHook() {
     override val key = "show_notification_history_and_log_entry"
     override fun hook() {
-        loadClass("com.android.settings.NotificationControlCenterSettings").methodFinder().filterByName("onCreate").first()
-            .createHook {
-                after {
-                    val thisObject = it.thisObject
-                    val resources = invokeMethodBestMatch(thisObject, "getResources") as Resources
-                    val notificationSettings = invokeMethodBestMatch(thisObject, "findPreference", null, "notification_settings")!!
-                    val preferenceManager = invokeMethodBestMatch(thisObject, "getPreferenceManager")!!
-                    val context = invokeMethodBestMatch(preferenceManager, "getContext") as Context
-                    val preferenceScreenForNotificationHistory = generatePreferenceScreenForNotificationHistory(context, resources)
-                    val preferenceScreenForNotificationLog = generatePreferenceScreenForNotificationLog(context, resources)
-                    invokeMethodBestMatch(notificationSettings, "addPreference", null, preferenceScreenForNotificationHistory)
-                    invokeMethodBestMatch(notificationSettings, "addPreference", null, preferenceScreenForNotificationLog)
+        val hook: HookFactory.() -> Unit = {
+            after {
+                val thisObject = it.thisObject
+                val resources = invokeMethodBestMatch(thisObject, "getResources") as Resources
+                val preferenceManager = invokeMethodBestMatch(thisObject, "getPreferenceManager")!!
+                val context = invokeMethodBestMatch(preferenceManager, "getContext") as Context
+                invokeMethodBestMatch(
+                    thisObject, "findPreference", null, "notification_settings"
+                )?.objectHelper {
+                    invokeMethodBestMatch(
+                        "addPreference", null, generatePreferenceScreen(
+                            context,
+                            resources,
+                            "com.android.settings.notification.history.NotificationHistoryActivity",
+                            "notification_history_title"
+                        )
+                    )
+                    invokeMethodBestMatch(
+                        "addPreference", null, generatePreferenceScreen(
+                            context,
+                            resources,
+                            "com.android.settings.Settings\$NotificationStationActivity",
+                            "notification_log_title"
+                        )
+
+                    )
                 }
             }
+        }
+        runCatching {
+            loadClass("com.android.settings.NotificationControlCenterSettings").methodFinder()
+                .filterByName("onCreate").first().createHook(hook)
+        }
+        runCatching {
+            loadClass("com.android.settings.NotificationStatusBarSettings").methodFinder()
+                .filterByName("onCreate").first().createHook(hook)
+        }
     }
 
     @SuppressLint("DiscouragedApi")
-    private fun generatePreferenceScreenForNotificationHistory(context: Context, resources: Resources): Any {
-        val preferenceScreenForNotificationHistory = loadClass("androidx.preference.PreferenceScreen").getDeclaredConstructor(
-            Context::class.java, AttributeSet::class.java
-        ).newInstance(context, null)
-        invokeMethodBestMatch(preferenceScreenForNotificationHistory, "setIntent", null, Intent().apply {
-            action = Intent.ACTION_MAIN
-            setClassName("com.android.settings", "com.android.settings.notification.history.NotificationHistoryActivity")
-        })
+    private fun generatePreferenceScreen(
+        context: Context, resources: Resources, className: String, titleIdName: String
+    ): Any {
+        val preferenceScreenForNotificationHistory =
+            loadClass("androidx.preference.PreferenceScreen").getDeclaredConstructor(
+                Context::class.java, AttributeSet::class.java
+            ).newInstance(context, null)
+        invokeMethodBestMatch(
+            preferenceScreenForNotificationHistory,
+            "setIntent",
+            null,
+            Intent().apply {
+                action = Intent.ACTION_MAIN
+                setClassName(
+                    "com.android.settings", className
+                )
+            })
         invokeMethodBestMatch(
             preferenceScreenForNotificationHistory,
             "setTitle",
             null,
-            resources.getIdentifier("notification_history_title", "string", hostPackageName)
+            resources.getIdentifier(titleIdName, "string", hostPackageName)
         )
         return preferenceScreenForNotificationHistory
-    }
-
-    @SuppressLint("DiscouragedApi")
-    private fun generatePreferenceScreenForNotificationLog(context: Context, resources: Resources): Any {
-        val preferenceScreenForNotificationLog = loadClass("androidx.preference.PreferenceScreen").getDeclaredConstructor(
-            Context::class.java, AttributeSet::class.java
-        ).newInstance(context, null)
-        invokeMethodBestMatch(preferenceScreenForNotificationLog, "setIntent", null, Intent().apply {
-            action = Intent.ACTION_MAIN
-            setClassName("com.android.settings", "com.android.settings.Settings\$NotificationStationActivity")
-        })
-        invokeMethodBestMatch(
-            preferenceScreenForNotificationLog,
-            "setTitle",
-            null,
-            resources.getIdentifier("notification_log_title", "string", hostPackageName)
-        )
-        return preferenceScreenForNotificationLog
     }
 }
