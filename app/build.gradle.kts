e59e2442e8811ee87ea364a7ec4d2e5c11e72c30
YifePlayte <62252171+YifePlayte@users.noreply.github.com>
2024-12-23 12:40:48 +0800

    Revert targetSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.yifeplayte.wommo"
         minSdk = 33
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
 
