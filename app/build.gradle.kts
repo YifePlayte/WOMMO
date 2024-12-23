1056940492799ec25480a0fa42f575ad805ac1e9
YifePlayte <62252171+YifePlayte@users.noreply.github.com>
2024-12-23 10:32:59 +0800

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.yifeplayte.wommo"
         minSdk = 33
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
 
