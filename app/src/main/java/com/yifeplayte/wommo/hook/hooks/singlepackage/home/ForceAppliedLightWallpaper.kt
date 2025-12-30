c618f6ecf2427db1da9655ce60dfe6426cbefa33
YifePlayte <62252171+YifePlayte@users.noreply.github.com>
2025-12-30 21:27:54 +0800

    Fix ForceAppliedLightWallpaper for HyperOS3

--- app/src/main/java/com/yifeplayte/wommo/hook/hooks/singlepackage/home/ForceAppliedLightWallpaper.kt
+++ app/src/main/java/com/yifeplayte/wommo/hook/hooks/singlepackage/home/ForceAppliedLightWallpaper.kt
@@ -1,6 +1,7 @@
 package com.yifeplayte.wommo.hook.hooks.singlepackage.home
 
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
+import com.github.kyuubiran.ezxhelper.ClassUtils.loadClassOrNull
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import com.yifeplayte.wommo.hook.hooks.BaseHook
@@ -11,11 +12,15 @@ object ForceAppliedLightWallpaper : BaseHook() {
     override fun hook() {
         val clazzWallpaperUtils = loadClass("com.miui.home.launcher.WallpaperUtils")
         clazzWallpaperUtils.methodFinder()
-            .filterByName("hasAppliedLightWallpaper").filterNonAbstract().single().createHook {
+            .filterByName("hasLightBgForStatusBar").filterNonAbstract().single().createHook {
                 returnConstant(true)
             }
         clazzWallpaperUtils.methodFinder()
-            .filterByName("hasLightBgForStatusBar").filterNonAbstract().single().createHook {
+            .filterByName("hasAppliedLightWallpaper").filterNonAbstract().singleOrNull()?.createHook {
+                returnConstant(true)
+            }
+        loadClassOrNull("com.miui.home.isolate.wallpaper.WallpaperUtil")?.methodFinder()
+            ?.filterByName("hasAppliedLightWallpaper")?.filterNonAbstract()?.single()?.createHook {
                 returnConstant(true)
             }
     }
