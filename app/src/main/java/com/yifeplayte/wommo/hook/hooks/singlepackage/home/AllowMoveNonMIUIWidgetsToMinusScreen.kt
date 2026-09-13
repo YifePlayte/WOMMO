1f86b1ff09f1060d5fa0685767bd4341c1d6f51a
YifePlayte <62252171+YifePlayte@users.noreply.github.com>
2026-09-13 22:40:17 +0800

    Update AllowMoveNonMIUIWidgetsToMinusScreen.kt

--- app/src/main/java/com/yifeplayte/wommo/hook/hooks/singlepackage/home/AllowMoveNonMIUIWidgetsToMinusScreen.kt
+++ app/src/main/java/com/yifeplayte/wommo/hook/hooks/singlepackage/home/AllowMoveNonMIUIWidgetsToMinusScreen.kt
@@ -1,23 +1,31 @@
 package com.yifeplayte.wommo.hook.hooks.singlepackage.home
 
+import com.yifeplayte.wommo.hook.hooks.BaseHook
+import com.yifeplayte.wommo.hook.utils.Log
 import io.github.lingqiqi5211.ezhooktool.core.callMethod
 import io.github.lingqiqi5211.ezhooktool.core.findMethod
 import io.github.lingqiqi5211.ezhooktool.core.getFieldOrNullAs
 import io.github.lingqiqi5211.ezhooktool.core.loadClass
 import io.github.lingqiqi5211.ezhooktool.xposed.dsl.createHook
 
-import com.yifeplayte.wommo.hook.hooks.BaseHook
-import com.yifeplayte.wommo.hook.utils.Log
-
 @Suppress("unused")
 object AllowMoveNonMIUIWidgetsToMinusScreen : BaseHook() {
     override val key = "allow_move_non_miui_widgets_to_minus_screen"
+    private val validSize by lazy {
+        setOf(
+            1 to 1,
+            2 to 1,
+            2 to 2
+        )
+    }
+
     override fun hook() {
         loadClass("com.miui.home.launcher.widget.MIUIWidgetHelper").findMethod {
             name("canDragToPa"); paramCount(2)
         }.createHook {
             before { param ->
                 runCatching {
+                    val clazzMIUIWidgetBasicInfo = loadClass("com.miui.home.launcher.MIUIWidgetBasicInfo")
                     val dragInfo = param.args[1]!!.callMethod("getDragInfo")!!
                     val spanX = dragInfo.getFieldOrNullAs<Int>("spanX")!!
                     val spanY = dragInfo.getFieldOrNullAs<Int>("spanY")!!
@@ -28,8 +36,11 @@ object AllowMoveNonMIUIWidgetsToMinusScreen : BaseHook() {
                         dragController.callMethod("isDraggingFromAssistant") as Boolean
                     val isDraggingToAssistant =
                         dragController.callMethod("isDraggingToAssistant") as Boolean
-                    param.result =
-                        launcherCallBacks != null && !isDraggingFromAssistant && !isDraggingToAssistant && spanX % 2 == 0 && (spanX != 2 || spanY == 2)
+                    param.result = clazzMIUIWidgetBasicInfo.isInstance(dragInfo)
+                            && launcherCallBacks != null
+                            && !isDraggingFromAssistant
+                            && !isDraggingToAssistant
+                            && (spanX == 4 || spanX to spanY in validSize)
                 }.onFailure {
                     Log.e("AllowMoveNonMIUIWidgetsToMinusScreen failed", it)
                 }
